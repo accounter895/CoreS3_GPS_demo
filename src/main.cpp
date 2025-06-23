b3ea12f7d3e544d967ea0b1213f80ee0ddca4321
accounter895 <ads7706879@gamil.com>
2025-06-23 16:23:02 +0800

    build: 更新平台依赖并重构代码
    - 更新 platformio.ini 文件，替换 M5Stack 和 M5AtomS3 为 M5Unified 和 M5GFX 库
    - 重构 main.cpp 文件，移除未使用的函数和注释，添加新的库引用

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,36 @@
 #include <Arduino.h>
+#include "M5Unified.h"
+#include "M5GFX.h"
+#include "MultipleSatellite.h"
 
 // put function declarations here:
-int myFunction(int, int);
+
+static const int RXPin = 5,TXPin = 6;
+static const uint32_t GPSBaud = 115200;
+MultipleSatellite gps(Serial1, GPSBaud, SERIAL_8N1, RXPin, TXPin);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    M5.begin();
+    M5.Power.begin();
+    M5.Display.setRotation(3);
+    M5.Display.fillScreen(BLACK);
+    
+    Serial1.begin(GPSBaud, SERIAL_8N1, RXPin, TXPin);
+    gps.begin();
+
+    // Set the GPS to a specific mode if needed
+    gps.setSatelliteMode(SATELLITE_MODE_GPS);
+    
+    // Optionally set the system boot mode
+    gps.setSystemBootMode(BOOT_HOST_START);
+
+    // Print GPS version
+    String version = gps.getGNSSVersion();
+    M5.Display.println("GPS Version: " + version);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
